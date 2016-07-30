8eeb45c651e4f4b6b32091b1bb8478fdea0f9fdf
Arnaud Balma <delwende.arnaud@gmail.com>
2016-07-30 10:38:55 -0500

    led test

--- first_test.js
+++ first_test.js
@@ -1,2 +1,17 @@
 // Turn on LED with Tessel 2
 
+var five = require('johnny-five');
+	Tessel = require('tessel-io');
+	board = new five.Board({
+		io: new Tessel()
+	});
+
+board.on("ready", () => {
+	var led = new five.Led("a5");
+
+	
+	//led.on();
+	//led.blink(2500)
+	led.blink();
+})
+
